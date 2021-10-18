076bf844a09e3fac7cde21237e9ad5254454b430
javaquero <joseantonio.vaquero@ezentis.com>
2021-10-18 13:08:39 +0200

    * Creamos: el componente basico de formulario y el componente de Input

---
+++ src/components/BasicForm.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>Formulario Basico</h1>
+    <BasicInput v-model="event.title" type="text" label="Titulo" />
+  </div>
+</template>
+
+<script>
+import BasicInput from "@/components/BasicInput";
+export default {
+  name: "BasicForm",
+  components: { BasicInput },
+  data() {
+    return {
+      event: {
+        category: "",
+        title: "",
+        description: "",
+        location: "",
+        pets: 1,
+        extras: {
+          catering: false,
+          music: false,
+        },
+      },
+    };
+  },
+  actions() {},
+};
+</script>
+
+<style scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <BasicForm />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import BasicForm from "../components/BasicForm";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    BasicForm,
   },
 };
 </script>
